7873c1cb90ee66f1d18fbb0a6358082e749f1095
jafarimahdi <mahdijafarii.mj@gmail.com>
2024-03-29 01:30:39 +0100

    ex01,02,03

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+void ft_print_alphabet(void)
+{
+	char alpha;
+	alpha = 'a';
+
+	while(alpha <= 'z')
+	{
+		write(1, &alpha, 1);
+		alpha ++;
+	}
+
+}
+
+int main(void)
+{
+	ft_print_alphabet();
+	return 0; 
+}
+
---
+++ C00/ex03/ft_print_numbers.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void ft_print_numbers(void):
+{
+	int num;
+	num = 0
+
+	while(num <= 9):
+	{
+		write(1,&num, 1);
+		num ++;
+	}
+}
+
+int main():
+{
+
+	ft_print_numbers();
+	return 0;
+}
